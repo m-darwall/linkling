09d1830641bd56a3c59cd699f7a25b98e10b9fa7
Martin <mdotdarwall@gmail.com>
2026-07-15 16:02:11 +0100

    Fetch words and randomly generate game(possibly unsolvable)

--- linkling.js
+++ linkling.js
@@ -1,12 +1,24 @@
-let words = [];
 
-function getWords(wordlist){
-    fetch(wordlist)
-        .then(response => response.text())
-        .then((data) => {
-            words = data.split("\r\n").slice(26, -1)
-            for (let i = 0; i < words.length; i++) {
-                words[i] = words[i].replace("$", "");
-            }
-        })
+async function getWords(wordlist){
+    let data = await fetch(wordlist);
+    let words = await data.text();
+    words = words.split("\r\n").slice(26, -1);
+    for (let i = 0; i < words.length; i++) {
+        words[i] = words[i].replace(/[^a-zA-Z0-9]/g, "");
+    }
+    return words;
+}
+
+function generatePuzzle(words){
+    let start = words[Math.floor(Math.random()*words.length)];
+    let target = words[Math.floor(Math.random()*words.length)];
+    while(target.length % 2 !== 0){
+        target = words[Math.floor(Math.random()*words.length)];
+    }
+    return [start, target];
+}
+
+async function setup(wordlist){
+    let words = await getWords(wordlist);
+    let puzzle = generatePuzzle(words);
 }
