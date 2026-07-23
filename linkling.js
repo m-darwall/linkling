8da53969fbc7627457572f6c4a3e9c1c93c6b300
Martin <mdotdarwall@gmail.com>
2026-07-23 14:23:34 +0100

    better unlocking style logic

--- linkling.js
+++ linkling.js
@@ -43,7 +43,8 @@ class Chain{
                 this.found.push(word.slice(-2));
                 let to_edit = document.getElementsByClassName("section_" + word.slice(-2))
                 for(let i = 0; i < to_edit.length; i++){
-                    to_edit[i].style.color = "green";
+                    // to_edit[i].style.color = "green";
+                    to_edit[i].classList.add("unlocked");
                 }
                 if(this.target.length === [...new Set(this.found)].length){
                     this.complete();
@@ -196,7 +197,8 @@ class Chain{
             if(this.found.includes(latest.slice(-2)) === false){
                 let to_edit = document.getElementsByClassName("section_" + latest.slice(-2))
                 for(let i = 0; i < to_edit.length; i++){
-                    to_edit[i].style.color = "#cfe0c3ff";
+                    // to_edit[i].style.color = "#cfe0c3ff";
+                    to_edit[i].classList.remove("unlocked")
                 }
             }
         }
