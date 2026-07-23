617df0b998ee37c5d0d43689165d9f08f3073b06
Martin <mdotdarwall@gmail.com>
2026-07-23 15:09:15 +0100

    update date function

--- linkling.js
+++ linkling.js
@@ -360,7 +360,7 @@ function format_date(offset){
     let today = new Date();
     today.setDate(today.getDate() + offset);
     let dd = String(today.getDate()).padStart(2, '0');
-    let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    let mm = String(today.getMonth() + 1).padStart(2, '0');
     let yyyy = today.getFullYear();
     return parseInt(dd+mm+yyyy)
 }
